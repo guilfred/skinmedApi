7ef59fc3d212b59c99629344ddfd2b2dfc24c148
guilfred <guilfred.assezo@gmail.com>
2025-09-07 20:09:14 +0000

    fix(Api): Logout action

--- app/controllers/security/logout_controller.ts
+++ app/controllers/security/logout_controller.ts
@@ -1,8 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LogoutController {
-  async logout({ auth, response }: HttpContext) {
+  async logout({ auth, response, session }: HttpContext) {
     await auth.use('api').logout()
+    session.clear()
 
     return response.status(200).json({ message: 'Deconnection du CRM !' })
   }
